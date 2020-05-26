6708193434fa051636805b34c812798f7551cbe5
Blackscure <Wekesabuyahi@gmail.com>
2020-05-26 10:04:35 +0300

    style added theme light

---
+++ app/src/main/java/com/buyahi/afya/AfyaActivity.java
@@ -0,0 +1,13 @@
+package com.buyahi.afya;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AfyaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_afya);
+    }
+}
