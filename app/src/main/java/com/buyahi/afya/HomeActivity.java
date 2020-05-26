049b34c20a21976935707743925d84453d098a1f
Blackscure <Wekesabuyahi@gmail.com>
2020-05-26 12:29:59 +0300

    text view added

--- app/src/main/java/com/buyahi/afya/HomeActivity.java
+++ app/src/main/java/com/buyahi/afya/HomeActivity.java
@@ -3,7 +3,12 @@ package com.buyahi.afya;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,6 +21,18 @@ public class HomeActivity extends AppCompatActivity {
     @BindView(R.id.logout)
     Button btnLogout;
     FirebaseAuth mFirebaseAuth;
+
+    @BindView(R.id.locationTextView)
+    TextView mLocationTextView;
+    @BindView(R.id.listView)
+    ListView mListView;
+    private String[] centers = new String[] {"Tassia", "Kitengela",
+            "Hulligam", "Garden City", "Thika", "Zion mall",
+            "Sinza cty", "Aurelx", "Maltys", "Andina",
+            "Lardo", "Portland City Grill", "Fat Head's Brewery",
+            "Chipotle", "Subway"};
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -32,5 +49,7 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
     }
+
+
 
 }
